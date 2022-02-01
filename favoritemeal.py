f590467ed8a205e77138645fff50421248b7d630
tundejolly74 <97757362+tundejolly74@users.noreply.github.com>
2022-01-31 17:37:32 -0800

    Update favoritemeal.py

--- favoritemeal.py
+++ favoritemeal.py
@@ -1,9 +1,10 @@
 # Favourite meal
 user_starter = input("What is your favourite starter? ")
 user_maincourse = input("What is your favourite main? ")
-user_drink = input("What is your favourite drink? ")
-user_desser = input("What is your favourite dessert? ")
+user_drink_desert = input("What is your favourite drink or desert? ")
 
-print("Your favourite meal is " + user_maincourse  + "with a glass of " + user_drink)
+
+print("Your favourite meal is " + user_maincourse  + " " + "with a glass of " + user_drink_desert)
+
 
 
