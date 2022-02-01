2383f56f3afbfdf78208f7e74e74596028b42efc
tundejolly74 <97757362+tundejolly74@users.noreply.github.com>
2022-01-31 17:34:41 -0800

    Update motorbike.py

--- motorbike.py
+++ motorbike.py
@@ -1,13 +1,12 @@
 # Motorbike Depreciation
-ProductName = "Motorbike"
-ProductPrice = 2000
-print(ProductName, ProductPrice)
-print("After 1 year ", ProductPrice - 200)
-print("After 2 years ", ProductPrice - 380)
-print("After 3 years ", ProductPrice - 542)
-print("After 4 years ", ProductPrice - 687.80)
-print("After 5 years ", ProductPrice - 819.02)
-print("After 6 years ", ProductPrice - 937.12)
-print("After 7 years ", ProductPrice - 1043.41)
+bike_cost= 2000.00
+cost= bike_cost
+year = 0
 
+print("yearly depreciation of motorbike value:")
+
+while cost >= 1000.00:
+    print("Year", year, ":", cost)
+    year = year + 1
+    cost = cost - (cost/10)
 
